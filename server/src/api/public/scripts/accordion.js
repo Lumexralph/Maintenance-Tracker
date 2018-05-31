f86467e0932cb4f3d735b1d13f33113e81693ec4
Ogundele Olumide <olumideralph@gmail.com>
2018-05-31 08:51:59 +0100

    initialize the creation of admin frontend
    feat(admin page): admin html page will be modified for easy
    insertion of data to thead and tbody.
    
    -	modified:   server/src/api/public/admin.html

--- server/src/api/public/scripts/accordion.js
+++ server/src/api/public/scripts/accordion.js
@@ -1,14 +1,17 @@
-const acc = document.getElementsByClassName("accordion");
+const acc = document.getElementsByClassName('accordion');
 
+/**
+ * Inspiration from https://www.w3schools.com/howto/howto_js_accordion.asp
+ */
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+      panel.style.maxHeight = `${panel.scrollHeight  }px`;
+    }
   });
 }
