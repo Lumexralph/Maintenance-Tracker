a391e803186706eeb22cb56c5659651d8e341e7e
Ogundele Olumide <olumideralph@gmail.com>
2018-06-05 06:16:46 +0100

    Restructure data of user requests returned
    fix(getAllUserRequest): the data structure is modified to return
    only relevant information and error messages are modified and
    made more helpful.
    
    -	modified:   server/controller/approveRequest.js
    -	modified:   server/controller/createUserRequest.js
    -	modified:   server/controller/getAllRequest.js
    -	modified:   server/controller/getAllUserRequest.js

--- server/controller/getAllRequest.js
+++ server/controller/getAllRequest.js
@@ -5,7 +5,7 @@ const getAllRequest = (req, res) => {
   const { filter } = req.query;
 
   if (!user.admin_role) {
-    return res.status(401).send({ message: 'Action cannot be performed' });
+    return res.status(401).send({ message: 'Only Admin is allowed to carry out the action.' });
   }
 
   if (filter) {
@@ -16,14 +16,14 @@ const getAllRequest = (req, res) => {
 
     return db.query(text)
       .then(result => res.status(200).send(result.rows))
-      .catch(err => res.status(400).send({ message: 'Request cannot be filtered' }));
+      .catch(err => res.status(400).send({ message: 'Requests cannot be filtered' }));
   }
 
   const text = 'SELECT * FROM requests ORDER BY request_id ASC;';
 
   return db.query(text)
     .then(result => res.status(200).send(result.rows))
-    .catch(err => res.status(501));
+    .catch(err => res.status(501).send({ message: 'Sorry, error occured in the system.' }));
 };
 
 export default getAllRequest;
--- server/controller/getAllUserRequest.js
+++ server/controller/getAllUserRequest.js
@@ -3,16 +3,17 @@ import db from '../db/index';
 const getAllUserRequests = (req, res) => {
   const { user } = req.body;
 
-  const text = `SELECT * FROM users INNER JOIN requests USING (user_id) WHERE user_id='${user.user_id}'`;
+  const text = `SELECT request_id, request_title, request_content, department, status
+   FROM users INNER JOIN requests USING (user_id) WHERE user_id='${user.user_id}'`;
 
   db.query(text)
     .then((result) => {
       if (result.rows.length > 0) {
-        return res.status(200).send({ message: result.rows });
+        return res.status(200).send(result.rows);
       }
-      return res.status(404).send({ message: 'No requests yet' });
+      return res.status(200).send({ message: 'You have not created any request yet' });
     })
-    .catch(err => res.status(404).send({ message: 'Request cannot be found' }));
+    .catch(err => res.status(404).send({ message: 'User requests cannot be found.' }));
 };
 
 export default getAllUserRequests;
