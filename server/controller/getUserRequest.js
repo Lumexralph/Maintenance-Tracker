4ced068114b5e002348e099ab9e28634fd07054a
Ogundele Olumide <olumideralph@gmail.com>
2018-06-11 17:45:50 +0100

    Create tests to get all user requests and a request
    chore(get requests endpoint): test to get all user requests
    endpoint is created which checks if the user is valid, has a
    token or not and if the user has requests created or not.
    Get a user request is created and checks if the id of request
    is created by user and when not created by the user.
    
    -	modified:   server/controller/getUserRequest.js
    -	modified:   server/tests/api.test.js
    -	modified:   server/tests/seed/seed.js
    
    Delivers [157561804, 157620645]

--- server/controller/getUserRequest.js
+++ server/controller/getUserRequest.js
@@ -5,7 +5,7 @@ const getUserRequest = (req, res) => {
   const { requestId } = req.params;
 
   const text = `SELECT requests.request_id, requests.request_title, requests.request_content, requests.department,
-  requests.status FROM users INNER JOIN requests USING (user_id) WHERE user_id= '${user.user_id}' AND request_id = '${requestId}';`;
+  requests.status FROM users INNER JOIN requests USING (user_id) WHERE user_id= '${user.userId}' AND request_id = '${requestId}';`;
 
   db.query(text)
     .then((result) => {
